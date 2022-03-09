7438d6625e6df4004ff2fa9dcd8031c20b6cae8c
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-09 10:29:29 -0500

    dice done

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,8 +1,20 @@
+import java.util.Random;
+
 public class Dice {
 
-    Integer numOfDice;
+    private int numOfDice;
+    private int sum;
+
     public Dice(int numOfDice) {
         this.numOfDice = numOfDice;
     }
 
+    public Integer tossAndSum() {
+        this.sum = 0;
+        Random random = new Random();
+        for (int i = 0; i < numOfDice; i++){
+            this.sum += random.nextInt(1,6);
+        }
+        return this.sum;
+    }
 }
