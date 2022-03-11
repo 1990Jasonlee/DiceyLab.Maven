c13d30d19a76fd0913faf4c3c16a725c474aedb0
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-10 22:18:15 -0500

    bins

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,11 @@
 
 public class Bins {
+    public int [] bin;
+    public Bins (int min , int max){
+        Integer[] bin = new Integer[max+1];
+        for (int i = 0; i < bin.length; i++){
+            bin[i] = 0;
 
+        }
+    }
 }
